29026eb54c343abd254f558846bc8e432e14feeb
chantz <chantzd52@gmail.com>
2024-04-25 10:18:04 -0700

    V0.27
    Added a high score menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,9 +17,16 @@ public class GameManager : MonoBehaviour
     public PowerUpSelection powerUpSelection;
 
     public TextMeshProUGUI timerText; // Reference to a TextMeshProUGUI component to display the timer
+    private float elapsedTime;
 
     private float startTime;
     private bool timerActive = false;
+
+    public TextMeshProUGUI highScore1Text;
+    public TextMeshProUGUI highScore2Text;
+    public TextMeshProUGUI highScore3Text;
+
+    public GameObject highScoresPanel;
 
     private GameObject currentPlayer;
 
@@ -29,7 +36,7 @@ public class GameManager : MonoBehaviour
         
         respawnButton.gameObject.SetActive(false);
         respawnButton.onClick.AddListener(RespawnPlayer);
-        
+        DisplayHighScores();
     }
 
     void Update()
@@ -92,14 +99,18 @@ public class GameManager : MonoBehaviour
             Destroy(powerup);
         }
 
+        highScoresPanel.SetActive(false); // Hide the high scores panel
         respawnButton.gameObject.SetActive(false);
+        
     }
 
         public void PlayerDied()
     {
 
         StopTimer();
+        CheckForHighScore(elapsedTime);
         powerUpSelection.HidePowerUpSelection();
+        highScoresPanel.SetActive(true); // Show the high scores panel
         respawnButton.gameObject.SetActive(true);
         
         // Handle other game over logic here
@@ -114,6 +125,7 @@ public class GameManager : MonoBehaviour
     private void StopTimer()
     {
         timerActive = false;
+        elapsedTime = Time.time - startTime;
     }
 
     private void ResetTimer()
@@ -134,8 +146,42 @@ public class GameManager : MonoBehaviour
     private string FormatTime(float time)
     {
         TimeSpan timeSpan = TimeSpan.FromSeconds(time);
-        return string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+        return string.Format("{0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
     }
+
+    private void CheckForHighScore(float score)
+    {
+        float highScore1 = PlayerPrefs.GetFloat("HighScore1", 0);
+        float highScore2 = PlayerPrefs.GetFloat("HighScore2", 0);
+        float highScore3 = PlayerPrefs.GetFloat("HighScore3", 0);
+
+        if (score > highScore1)
+        {
+            PlayerPrefs.SetFloat("HighScore3", highScore2);
+            PlayerPrefs.SetFloat("HighScore2", highScore1);
+            PlayerPrefs.SetFloat("HighScore1", score);
+        }
+        else if (score > highScore2)
+        {
+            PlayerPrefs.SetFloat("HighScore3", highScore2);
+            PlayerPrefs.SetFloat("HighScore2", score);
+        }
+        else if (score > highScore3)
+        {
+            PlayerPrefs.SetFloat("HighScore3", score);
+        }
+
+        DisplayHighScores();
+    }
+
+     private void DisplayHighScores()
+    {
+        highScore1Text.text = "1st: " + FormatTime(PlayerPrefs.GetFloat("HighScore1", 0));
+        highScore2Text.text = "2nd: " + FormatTime(PlayerPrefs.GetFloat("HighScore2", 0));
+        highScore3Text.text = "3rd: " + FormatTime(PlayerPrefs.GetFloat("HighScore3", 0));
+    }
+
+   
 
     public void RespawnPlayer()
     {
